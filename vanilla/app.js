9971abcd200f499187205f3f652f3a64986dccf0
jcfn19 <jcfnorstrand@outlook.com>
2024-04-08 14:14:29 +0200

    udated code

--- vanilla/app.js
+++ vanilla/app.js
@@ -24,11 +24,27 @@ const path = require('path');
 const publicDirectoryPath = path.join(__dirname, "./static")
 app.use(express.static(publicDirectoryPath))
 
+// const userAgent = navigator.userAgent;
+// let operatingSystem = "Unknown";
+// if (userAgent.indexOf("Windows") !== -1) {
+//   operatingSystem = "Windows";
+// } else if (userAgent.indexOf("Mac") !== -1) {
+//   operatingSystem = "Mac OS";
+// } else if (userAgent.indexOf("Linux") !== -1) {
+//   operatingSystem = "Linux";
+// } else if (userAgent.indexOf("Android") !== -1) {
+//   operatingSystem = "Android";
+// } else if (userAgent.indexOf("iOS") !== -1) {
+//   operatingSystem = "iOS";
+// }
+// console.log("Operating System:", operatingSystem);
+
 function formhandlermelding(request, response) {
+    let url = request.url
     console.log(request.body);
-    const msql = db.prepare('INSERT INTO fmelding (message, time) VALUES (?, ?)');
+    const msql = db.prepare('INSERT INTO fmelding (message, time, url) VALUES (?, ?, ?)');
     console.log(window.location.href)
-    const info = msql.run(request.body.tilbakemelding, tid);//document.referrer/window.location.href for url
+    const info = msql.run(request.body.tilbakemelding, tid, url);//window.location.href for url
 
     response.send("feedback sendt")
 }
