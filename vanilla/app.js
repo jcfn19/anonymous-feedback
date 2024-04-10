92161cf85a450c240d0bbeac81da679e09e1efba
jcfn19 <jcfnorstrand@outlook.com>
2024-04-10 14:15:15 +0200

    updated code

--- vanilla/app.js
+++ vanilla/app.js
@@ -5,7 +5,6 @@ const app = express();
 app.use(useragent.express());
 const sqlite3 = require('better-sqlite3');
 const db = sqlite3('feedbackv.db', {verbose: console.log})
-const bodyParser = require('body-parser');
 
 const currentTime = new Date();
 
@@ -13,8 +12,8 @@ const currentTime = new Date();
 let tid = currentTime.getHours() + ":" + currentTime.getMinutes() + ":" + currentTime.getSeconds();
 // console.log("Hello World!");
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 function rootRoute(request, response) {
     const sql = db.prepare('SELECT * FROM fmelding');
@@ -35,20 +34,17 @@ app.get('/fos', (req, res) => {
     const os = userAgent.os;
     
     // res.send('Operating System: ' + os);
-    console.log(JSON.stringify(os))
-    // let op = os
-    // console.log(op)
+   console.log(JSON.stringify(os))
 });
 
 function formhandlermelding(request, response, req, res) {
     const userAgent = req.useragent;
     const os = userAgent.os;
     // JSON.stringify(os);
-    // let op = os;
     console.log(request.body);
     const msql = db.prepare('INSERT INTO fmelding (message, time, os) VALUES (?, ?, ?)');
-    // console.log(window.location.href) for url
-    const info = msql.run(request.body.tilbakemelding, tid, os);
+    // console.log(window.location.href)
+    const info = msql.run(request.body.tilbakemelding, tid, JSON.parse(os));//res.send('Operating System: ' + os)
 
     response.send("feedback sendt")
 }
