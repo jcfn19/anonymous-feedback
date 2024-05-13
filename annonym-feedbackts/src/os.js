5c6a4d41188bf968db81dd159db042a4b30b96f4
jcfn19 <jcfnorstrand@outlook.com>
2024-05-13 13:36:33 +0200

    updated code

--- annonym-feedbackts/src/os.js
+++ annonym-feedbackts/src/os.js
@@ -10,27 +10,42 @@ let urlarray = [url];
 
 // if statement to prevent spaming of the same button
 
-if (sendjson.body.feedback == null) {
-    function sendjson() {
-        const body = {
-            jsonform: osarray,
-            feedback: document.querySelector("#feedbackid").value,
-            currenturl: urlarray,
-        }
-    
-        fetch("/sendmap", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(body)
-        })
+async function preventspamf() {
+    const response = await fetch("/feedbackrawjson");
+    const data = await response.json();
+    console.log(data);
+
+    let tfeedback = data[0].afeedbackm;
+    console.log(tfeedback);
+
+    const body = {
+        jsonform: osarray,
+        feedback: document.querySelector("#feedbackid").value,
+        currenturl: urlarray,
     }
-    
-    document.querySelector("#submitbtn").addEventListener("click", sendjson);
-} else {
-    return console.log("you have already submitted");
+
+    for (let i = 0; i < data.length; i++) {
+        if (body.feedback == data[i].afeedbackm) {
+            console.log("can't submit, data already exists");
+            break;
+        } else {
+            function sendjson() {
+            
+                fetch("/sendmap", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(body)
+                })
+            }
+            
+            document.querySelector("#submitbtn").addEventListener("click", sendjson);
+        }
+    }
 }
+
+preventspamf()
 
 // Event.preventDefault() //will need if statement for submit
 // this.countUpdate.emit((<HTMLTextAreaElement>e.target).value./*...*/)
