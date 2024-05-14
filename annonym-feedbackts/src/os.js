bb62fab3fe8dad229d1202af7820258ad9e2968f
jcfn19 <jcfnorstrand@outlook.com>
2024-05-14 13:49:08 +0200

    updated code

--- annonym-feedbackts/src/os.js
+++ annonym-feedbackts/src/os.js
@@ -15,45 +15,38 @@ async function preventspamf() {
     const data = await response.json();
     console.log(data);
 
-    let tfeedback = data[0].afeedbackm;
-    console.log(tfeedback);
+    // let tfeedback = data[0].afeedbackm;
+    // console.log(tfeedback);
+
+    let feedbackdata;
+
+    for (let i = 0; i < data.length; i++) {
+        feedbackdata = data[i].afeedbackm;
+        console.log(feedbackdata);
+    }
 
     const body = {
         jsonform: osarray,
         feedback: document.querySelector("#feedbackid").value,
         currenturl: urlarray,
     }
-
-    for (let i = 0; i < data.length; i++) {
-        if (body.feedback == data[i].afeedbackm) {
-            console.log("can't submit, data already exists");
-            break;
-        } else {
-            function sendjson() {
-            
-                fetch("/sendmap", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    body: JSON.stringify(body)
-                })
-            }
-            
-            document.querySelector("#submitbtn").addEventListener("click", sendjson);
+    
+    if (body.feedback == feedbackdata) {
+        console.log("can't submit, data already exists");
+    } else {
+        function sendjson() {
+        
+            fetch("/sendmap", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(body)
+            })
         }
+        
+        document.querySelector("#submitbtn").addEventListener("click", sendjson);
     }
 }
 
-preventspamf()
-
-// Event.preventDefault() //will need if statement for submit
-// this.countUpdate.emit((<HTMLTextAreaElement>e.target).value./*...*/)
-
-// function checkboxClick(event) {
-//     let warn = "preventDefault() won't let you check this!<br>";
-//     document.getElementById("output-box").innerHTML += warn;
-//     event.preventDefault();
-// }
-
-// document.getElementById("submitbtn").disabled = false;
+// document.querySelector("#submitbtn").addEventListener("click", preventspamf);
