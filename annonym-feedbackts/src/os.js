90cb20cdaf7f8790aaab15f8d0696a0c17d2474f
jcfn19 <jcfnorstrand@outlook.com>
2024-05-22 11:36:22 +0200

    updated code. version before pen test

--- annonym-feedbackts/src/os.js
+++ annonym-feedbackts/src/os.js
@@ -1,5 +1,56 @@
 // to get os will use json or map
 
+// const os = window.navigator.userAgent
+
+// const url = window.location.href
+
+// let osmap = new Map();
+// let osarray = [os];
+// let urlarray = [url];
+
+// if statement to prevent spaming of the same button
+
+// async function preventspamf() {
+//     const response = await fetch("/feedbackrawjson");
+//     const data = await response.json();
+//     console.log(data);
+
+    // let tfeedback = data[0].afeedbackm;
+    // console.log(tfeedback);
+
+//     let feedbackdata;
+
+//     for (let i = 0; i < data.length; i++) {
+//         feedbackdata = data[i].afeedbackm;
+//         console.log(feedbackdata);
+//     }
+
+//     const body = {
+//         jsonform: osarray,
+//         feedback: document.querySelector("#feedbackid").value,
+//         currenturl: urlarray,
+//     }
+    
+//     if (body.feedback == feedbackdata) {
+//         console.log("can't submit, data already exists");
+//     } else {
+//         function sendjson() {
+        
+//             fetch("/sendmap", {
+//                 method: "POST",
+//                 headers: {
+//                     "Content-Type": "application/json"
+//                 },
+//                 body: JSON.stringify(body)
+//             })
+//         }
+        
+//         document.querySelector("#submitbtn").addEventListener("click", sendjson);
+//     }
+// }
+
+// document.querySelector("#submitbtn").addEventListener("click", preventspamf);
+
 const os = window.navigator.userAgent
 
 const url = window.location.href
@@ -8,45 +59,20 @@ let osmap = new Map();
 let osarray = [os];
 let urlarray = [url];
 
-// if statement to prevent spaming of the same button
-
-async function preventspamf() {
-    const response = await fetch("/feedbackrawjson");
-    const data = await response.json();
-    console.log(data);
-
-    // let tfeedback = data[0].afeedbackm;
-    // console.log(tfeedback);
-
-    let feedbackdata;
-
-    for (let i = 0; i < data.length; i++) {
-        feedbackdata = data[i].afeedbackm;
-        console.log(feedbackdata);
-    }
-
+function sendjson() {
     const body = {
         jsonform: osarray,
         feedback: document.querySelector("#feedbackid").value,
         currenturl: urlarray,
     }
-    
-    if (body.feedback == feedbackdata) {
-        console.log("can't submit, data already exists");
-    } else {
-        function sendjson() {
-        
-            fetch("/sendmap", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(body)
-            })
-        }
-        
-        document.querySelector("#submitbtn").addEventListener("click", sendjson);
-    }
+
+    fetch("/sendmap", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    })
 }
 
-// document.querySelector("#submitbtn").addEventListener("click", preventspamf);
+document.querySelector("#submitbtn").addEventListener("click", sendjson);
